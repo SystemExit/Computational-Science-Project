7df9d1f812c15a37bbf807077ab3970021f4199e
anne beks <anne.beks@student.uva.nl>
2026-01-30 21:05:34 +0100

    edit file names
    Refactor plotting functionality and ensure plots are saved in 'plots' directory.

--- plots3_people.py
+++ plots3_people.py
@@ -4,6 +4,9 @@ import glob
 import os
 import numpy as np
 
+# make sure 'plots' directory exists to safe plots in
+os.makedirs('plots', exist_ok=True)
+
 # Find files
 csv_files = glob.glob("data/sim_results/**/*.csv", recursive=True)
 all_results = []
@@ -86,30 +89,27 @@ def plot_group_with_bounds(modes_to_plot, title, filename, legend_outside=False)
     plt.title(title, fontsize=18, fontweight='bold', pad=15)
     plt.xlabel('PrEP Coverage (Amount of people)', fontsize=14)
     plt.ylabel('Final Healthy Population (Susceptible Count)', fontsize=14)
-    # plt.xticks(np.arange(0, 1000, 0.1))
-    # plt.xlim(-0.02, 1.02)
     plt.grid(True, linestyle='--', alpha=0.5)
     plt.legend(loc='upper left', fontsize=12, frameon=True, shadow=True)
     plt.tight_layout()
     plt.savefig(filename, dpi=300, bbox_inches='tight')
-    print(f"Opgeslagen: {filename}")
+    print(f"Saved: {filename}")
     plt.show()
 
-# Plot 1: Gender
-# group_1 = ['random', 'targeted_male', 'targeted_female']
+
+# Plots grouped by scale of x-axis
+# Plot 1:
 group_1 = ['random', 'targeted_heterosexual']
-plot_group_with_bounds(group_1, "PrEP Effectiveness: Random vs. Heterosexual", "final_susc_large.png")
+plot_group_with_bounds(group_1, "PrEP Effectiveness: Random vs. Heterosexual", "plots/prep_effect_largest_scale.png")
 
-# Plot 2: Sexual Orientation
-# group_2 = ['targeted_heterosexual', 'targeted_homosexual', 'targeted_bisexual']
+# Plot 2: 
 group_2 = ['targeted_male', 'targeted_female', 'targeted_m_hetero', 'targeted_f_hetero']
-plot_group_with_bounds(group_2, "PrEP Effectiveness: (heterosexual) male vs. female", "final_susc_medium.png")
+plot_group_with_bounds(group_2, "PrEP Effectiveness: (heterosexual) male vs. female", "plots/prep_effect_medium_scale.png")
 
-# Plot 3: Six supgroups
-# group_3 = ['targeted_m_homo', 'targeted_m_hetero', 'targeted_m_bi',
-#            'targeted_f_homo', 'targeted_f_hetero', 'targeted_f_bi']
+# Plot 3:
 group_3 = ['targeted_m_homo', 'targeted_m_bi',
            'targeted_f_homo', 'targeted_f_bi',
            'targeted_homosexual', 'targeted_bisexual']
-plot_group_with_bounds(group_3, "PrEP Effectiveness: bisexual vs. homosexual", "final_susc_small.png")
+plot_group_with_bounds(group_3, "PrEP Effectiveness: bisexual vs. homosexual", "plots/prep_effect_small_scale.png")
+
 
