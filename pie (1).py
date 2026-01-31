3e9a25bafcebd137c4b991e06004047765921ceb
anne beks <anne.beks@student.uva.nl>
2026-01-31 14:56:56 +0100

    Add files via upload

---
+++ pie (1).py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+
+labels = ['m_homo', 'm_hetero', 'm_bi', 'f_homo', 'f_hetero', 'f_bi']
+values = [30, 431, 21, 10, 468, 40]
+
+colors = [
+    '#145f82',  # m_homo
+    '#ff7f0e',  # m_hetero
+    '#186c24',  # m_bi
+    '#0f9ed5',  # f_homo
+    '#a02b93',  # f_hetero
+    '#4ea72e'   # f_bi
+]
+
+plt.figure(figsize=(6, 4))
+plt.pie(values, colors=colors, startangle=90)
+plt.title('Population Distribution')
+
+plt.legend(labels, loc='lower center', ncol=6, bbox_to_anchor=(0.5, -0.15))
+plt.show()
